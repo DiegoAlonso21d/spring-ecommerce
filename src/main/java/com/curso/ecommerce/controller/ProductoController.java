752e367a46317d1714c7723fbc9a8c17328ba96f
Diego Alonso <diegoalonso_21d@hotmail.com>
2023-05-09 18:00:56 -0500

    elimiar registro producto

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -89,6 +89,15 @@ public class ProductoController {
 	}
 	
 	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable Integer id) {
+
+			
+		productoService.delete(id);
+		
+		return "redirect:/productos";
+		
+	}
 	
 
 	
