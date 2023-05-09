7804a5ebb1580bae0693d31fd02c3d6eaebfa7fe
Diego Alonso <diegoalonso_21d@hotmail.com>
2023-05-09 15:20:45 -0500

    Funcionalidad de guardar producto

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -1,8 +1,15 @@
 package com.curso.ecommerce.controller;
 
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.curso.ecommerce.model.Producto;
+import com.curso.ecommerce.model.Usuario;
+import com.curso.ecommerce.service.ProductoService;
 
 @Controller
 @RequestMapping("/productos")
@@ -10,13 +17,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProductoController {
 
 	
+	private final Logger logger=LoggerFactory.getLogger(ProductoController.class);
+	
+	@Autowired
+	private ProductoService productoService;
+	
 	
 	@GetMapping("")
 	public String show() {
 		return "productos/show";
 	}
 	
+	@GetMapping("/create")
+	public String create() {
+		return "productos/create";
+	}
 	
 	
+	
+	
+	
+	@PostMapping("/save")
+	public String save(Producto producto) {
+	
+		
+		logger.info("Este es el objeto de la lista {}",producto);
+		
+		Usuario u =new Usuario(1, "", "", "", "", "", "", "");
+		producto.setUsuario(u);
+		productoService.save(producto);
+		
+		
+		return "redirect:/productos";
+	}
+	
+	
+
 	
 }
